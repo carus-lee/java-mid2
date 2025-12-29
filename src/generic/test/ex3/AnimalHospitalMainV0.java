cefe4abad3709c0f2bf1377f2a3ca10dd8f9daed
carus-lee <leec8.2019@gmail.com>
2025-12-30 08:08:55 +0900

    Generic2 - 타입 매개변수 제한  > 개병원, 고양이병원

---
+++ src/generic/test/ex3/AnimalHospitalMainV0.java
@@ -0,0 +1,31 @@
+package generic.test.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV0 {
+    public static void main(String[] args) {
+        DogHospital dogHospital = new DogHospital();
+        CatHospital catHospital = new CatHospital();
+
+        Dog dog = new Dog("멍멍이1", 100);
+        Cat cat = new Cat("야옹이1", 300);
+
+        // 개병원
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        // 개병원
+        catHospital.set(cat);
+        catHospital.checkup();
+
+        // 문제1: 개병원에 고양이 전달
+        // dogHospital.set(cat); // 컴파일 오류 (다른타입 입력)
+
+        // 문제2: 개 타입 반환
+        dogHospital.set(dog);
+        Dog biggerDog = dogHospital.bigger(new Dog("멍멍이2", 200));
+        System.out.println("bigger Dog = "+ biggerDog);
+    }
+
+}
