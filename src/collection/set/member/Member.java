7035d0e4515773c3699cdef4e85bcb3f5fbf9a0b
carus-lee <leec8.2019@gmail.com>
2026-03-09 12:39:37 +0900

    컬렉션 프레임워크 - HashSet - 직접 구현하는 Set3 - 직접 만든 객체 보관 (member)

--- src/collection/set/member/Member.java
+++ src/collection/set/member/Member.java
@@ -26,4 +26,11 @@ public class Member {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                '}';
+    }
 }
